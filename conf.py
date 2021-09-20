eedd48a30b5330b54b210573fa9298de844a2d50
wdmhouston <wangdeming@gmail.com>
2021-09-19 19:53:44 -0500

    add theme

--- conf.py
+++ conf.py
@@ -1,3 +1,5 @@
+import sphinx_rtd_theme
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -47,11 +49,13 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-
 #html_theme = 'alabaster'
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
+# html_theme = 'sphinxdoc'
+html_theme = 'sphinx_rtd_theme'
+
+#html_theme_path = []
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- index.rst
+++ index.rst
@@ -1,5 +1,5 @@
 .. ProjectName documentation master file, created by
-   sphinx-quickstart on Sun Sep 19 19:20:58 2021.
+   sphinx-quickstart on Sun Sep 19 19:50:13 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
